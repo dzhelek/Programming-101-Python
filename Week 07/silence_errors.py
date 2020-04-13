3896369be78034515ef8f43e820875825d39fb76
dzhelek <yoan.dzhelekarski@gmail.com>
2020-04-13 19:16:29 +0300

    implement silence_errors with class context manager

--- Week 07/silence_errors.py
+++ Week 07/silence_errors.py
@@ -8,3 +8,22 @@ def silence_exception(exception, msg=None):
     except exception as exc:
         if msg is not None and msg != str(exc):
             raise
+
+
+class silence_exception:
+    def __init__(self, exception, msg=None):
+        self.exception = exception
+        self.msg = msg
+        self.raising = False
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if self.exception == exc_type:
+            if self.msg is not None and self.msg != str(exc_value):
+                return False
+            else:
+                return True
+        else:
+            return False
