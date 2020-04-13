27b21634b2b777e0cb0d8dbd7685c4aa41c1c9f0
dzhelek <yoan.dzhelekarski@gmail.com>
2020-04-13 19:48:50 +0300

    implement change_precision class context manager

--- Week 07/change_precision.py
+++ Week 07/change_precision.py
@@ -12,3 +12,16 @@ def change_precision(precision):
         raise
     finally:
         getcontext().prec = default_precision
+
+class change_precision:
+    def __init__(self, precision):
+        self.precision = precision
+
+    def __enter__(self):
+        self.default_precision = getcontext().prec
+        getcontext().prec = self.precision
+
+        return self
+
+    def __exit__(self, *exc_args):
+        getcontext().prec = self.default_precision
